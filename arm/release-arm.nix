be5c23cd90d21314437be4d9cc14aec1c314cf32
michael bishop <cleverca22@gmail.com>
2017-06-21 05:35:56 -0300

    and set the systems arg

--- arm/release-arm.nix
+++ arm/release-arm.nix
@@ -1,6 +1,9 @@
 { nixpkgs }:
 
-with import <nix/release.nix> { inherit nixpkgs; };
+with import <nix/release.nix> {
+  inherit nixpkgs;
+  systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "armv6l-linux" "armv7l-linux" ];
+};
 
 {
   inherit build binaryTarball coverage tarball;
