fbc4ef7a322e1218af53ced4ec5c30be6622c409
michael bishop <cleverca22@gmail.com>
2017-06-22 21:51:45 -0300

    add some example json and do pr builds for toxvpn

--- toxvpn/default.nix
+++ toxvpn/default.nix
@@ -1,8 +1,9 @@
-{ pulls }:
+{ pulls ? ../simple-pr-dummy.json }:
 
 let
   pkgs = import <nixpkgs>{};
 in with (import ../lib.nix { inherit pkgs; });
+with pkgs.lib;
 let
   defaults = {
     enabled = 1;
@@ -12,19 +13,31 @@ let
     checkinterval = 60;
     enableemail = false;
     emailoverride = "";
+    nixexprinput = "toxvpn";
+    nixexprpath = "release.nix";
   };
-  jobsetsAttrs = {
+  primary_jobsets = {
     toxvpn-unstable = defaults // {
       description = "toxvpn";
-      nixexprinput = "toxvpn";
-      nixexprpath = "release.nix";
       inputs = {
         toxvpn = mkFetchGithub "https://github.com/cleverca22/toxvpn master";
-        test = { type = "path"; value = pulls; emailresponsible = false; };
         nixpkgs = mkFetchGithub "https://github.com/nixos/nixpkgs-channels.git nixos-unstable-small";
       };
     };
   };
+  pr_data = builtins.fromJSON (builtins.readFile pulls);
+  makePr = num: info: {
+    name = "toxvpn-${num}";
+    value = defaults // {
+      description = "PR ${num}: ${info.title}";
+      inputs = {
+        toxvpn = mkFetchGithub "https://github.com/${info.head.repo.owner.login}/${info.head.repo.name}.git ${info.head.ref}";
+        nixpkgs = mkFetchGithub "https://github.com/nixos/nixpkgs-channels.git nixos-unstable-small";
+      };
+    };
+  };
+  pull_requests = listToAttrs (mapAttrsToList makePr pr_data);
+  jobsetsAttrs = pull_requests // primary_jobsets;
 in {
   jobsets = pkgs.writeText "spec.json" (builtins.toJSON jobsetsAttrs);
 }
