3e2caffc474ea93b98db26e28b604266099e26ba
michael bishop <cleverca22@gmail.com>
2017-06-24 21:09:59 -0300

    turn aarch64 builds off, no build slaves are configured

--- arm/nix/release-arm.nix
+++ arm/nix/release-arm.nix
@@ -2,7 +2,7 @@
 
 with import <nix/release.nix> {
   inherit nixpkgs;
-  systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "armv6l-linux" "armv7l-linux" ];
+  systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "armv6l-linux" "armv7l-linux" ];
 };
 
 {
