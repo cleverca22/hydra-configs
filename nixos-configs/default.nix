084ebe9e67d764baa2be45451f0ff0ef7f290fc6
Michael Bishop <cleverca22@gmail.com>
2018-07-16 23:56:40 -0300

    add nixos-configs

---
+++ nixos-configs/default.nix
@@ -0,0 +1,23 @@
+{ }:
+
+let
+  pkgs = import <nixpkgs> {};
+in with (import ../lib.nix { inherit pkgs; });
+with pkgs.lib;
+let
+  defaults = globalDefaults // {
+    nixexprinput = "nixos-configs";
+    nixexprpath = "release.nix";
+    checkinterval = 600;
+  };
+  nixos-configs = defaults // {
+    description = "nixos-configs";
+    inputs = {
+      nixos-configs = mkFetchGithub "https://github.com/cleverca22/nixos-configs master";
+      nixpkgs = mkFetchGithub "https://github.com/nixos/nixpkgs-channels.git nixos-unstable";
+    };
+  };
+  jobsetsAttrs = { inherit nixos-configs; };
+in {
+  jobsets = pkgs.writeText "spec.json" (builtins.toJSON jobsetsAttrs);
+}
