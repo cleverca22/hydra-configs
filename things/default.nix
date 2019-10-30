e9a6e113a594501086d165b1fd9e405f3b6650fb
Michael Bishop <cleverca22@gmail.com>
2019-10-30 17:45:46 -0300

    change channel and reduce keepnr

--- things/default.nix
+++ things/default.nix
@@ -10,11 +10,12 @@ let
     checkinterval = 600;
   };
   cachecache = defaults // {
+    keepnr = 3;
     nixexprinput = "cachecache";
     description = "a nix binary cache cache";
     inputs = {
       cachecache = mkFetchGithub "https://github.com/cleverca22/cachecache master";
-      nixpkgs = mkFetchGithub "https://github.com/nixos/nixpkgs-channels.git nixos-unstable-small";
+      nixpkgs = mkFetchGithub "https://github.com/nixos/nixpkgs-channels.git nixos-unstable";
     };
   };
   jobsetsAttrs = {
