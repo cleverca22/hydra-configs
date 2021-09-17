a18d66d25aa74d1e68f85965e3b637865a6ede1c
Michael Bishop <cleverca22@gmail.com>
2021-09-17 01:04:27 -0300

    patch libffi expr

--- arm32/release.nix
+++ arm32/release.nix
@@ -1,4 +1,11 @@
-{
+let
+  hostPkgs = import <nixpkgs> {};
+  patchedPkgs = hostPkgs.runCommand "nixpkgs" { patches = [ ./libffi.patch ]; } ''
+    cp -rL ${<nixpkgs>} $out
+    cd $out
+    patchPhase
+  '';
+in {
   armv7l-linux = let
     overlay = self: super: {
       libunwind = super.libunwind.overrideAttrs (old: {
@@ -7,14 +14,14 @@
       python = super.python.overrideAttrs (old: {
         configureFlags = old.configureFlags ++ [ "--with-system-ffi" ];
       });
-      pythonPackages = self.callPackage (<nixpkgs/pkgs/top-level/python-packages.nix>) {
-        python = self.python;
-      };
+      #pythonPackages = self.callPackage (<nixpkgs/pkgs/top-level/python-packages.nix>) {
+      #  python = self.python;
+      #};
     };
-    pkgs = import <nixpkgs> { system = "armv7l-linux"; overlays = [ overlay ]; };
+    pkgs = import patchedPkgs { system = "armv7l-linux"; overlays = [ overlay ]; };
   in {
     inherit (pkgs) stdenv libunwind libffi python;
-    pythonPackages = {
+    pythonPackages2 = {
       inherit (pkgs.pythonPackages) setuptools;
     };
   };
