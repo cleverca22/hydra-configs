d8aaf112d9739dc32f345ed61b02281e53d0fe2a
Michael Bishop <cleverca22@gmail.com>
2021-09-16 22:32:08 -0300

    fix libunwind

--- arm32/release.nix
+++ arm32/release.nix
@@ -1,6 +1,11 @@
 {
   armv7l-linux = let
-    pkgs = import <nixpkgs> { system = "armv7l-linux"; };
+    overlay = self: super: {
+      libunwind = super.libunwind.overrideAttrs (old: {
+        configureFlags = old.configureFlags ++ [ "--target=armv7l-linux" ];
+      });
+    };
+    pkgs = import <nixpkgs> { system = "armv7l-linux"; overlays = [ overlay ]; };
   in {
     inherit (pkgs) stdenv libunwind;
   };
