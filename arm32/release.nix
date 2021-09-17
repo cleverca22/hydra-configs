923d02a1c0efd42dacdbbaa7b7804b4b4f0d132b
Michael Bishop <cleverca22@gmail.com>
2021-09-16 23:10:33 -0300

    build python with system ffi

--- arm32/release.nix
+++ arm32/release.nix
@@ -4,10 +4,16 @@
       libunwind = super.libunwind.overrideAttrs (old: {
         configureFlags = old.configureFlags ++ [ "--target=armv7l-linux" ];
       });
+      python = super.python.overrideAttrs (old: {
+        configureFlags = old.configureFlags ++ [ "--with-system-ffi" ];
+      });
+      pythonPackages = self.callPackage ("${self.path}/pkgs/top-level/python-packages.nix") {
+        python = self.python;
+      };
     };
     pkgs = import <nixpkgs> { system = "armv7l-linux"; overlays = [ overlay ]; };
   in {
-    inherit (pkgs) stdenv libunwind libffi;
+    inherit (pkgs) stdenv libunwind libffi python;
     pythonPackages = {
       inherit (pkgs.pythonPackages) setuptools;
     };
